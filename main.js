d4294dae9d2dc134f458378e82899afa8d0a2f1e
jonahhess <jonahhessdev@gmail.com>
2025-07-27 16:18:19 +0300

    driver code

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 import Tweeter from "./model.js";
+import renderPosts from "./render.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const model = new Tweeter();
+  renderPosts(model.getPosts);
+});
