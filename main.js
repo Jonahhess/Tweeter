93cd65330f1a09943e243e78e8de05a28ed0eb5c
jonahhess <jonahhessdev@gmail.com>
2025-07-27 17:40:55 +0300

    added submit post

--- main.js
+++ main.js
@@ -3,5 +3,14 @@ import renderPosts from "./render.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   const model = new Tweeter();
-  renderPosts(model.getPosts);
+  renderPosts(model.getPosts());
+
+  const twit = document.getElementById("submit-btn");
+  twit.onclick = () => {
+    const input = document.getElementById("input");
+    const text = input.value;
+    input.value = "";
+    model.addPost(text);
+    renderPosts(model.getPosts());
+  };
 });
